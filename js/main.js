8ed051c544478fc4f93dd7c9f446a01e2d7300f4
bocchilorenzo <lorenzobocchi99@yahoo.com>
2020-04-20 22:31:54 +0200

    Major changes
    Changed library for Base64, temporarily removed live encode/decode, added working tab navigation

--- js/main.js
+++ js/main.js
@@ -1,22 +1,86 @@
-//Controllare codifica del testo??
+var eventEncode = true;
+
 function encode() {
     var str = document.getElementById("text").value;
-    document.getElementById("result").value = Base64.encode(str);
+    document.getElementById("result").value = btoaUTF8(str, autoBOMit = false);
 }
 
 function decode() {
     var str = document.getElementById("text").value;
-    document.getElementById("result").value = Base64.decode(str);
+    document.getElementById("result").value = atobUTF8(str, keepBOM = false);
 }
 
-function liveEncodeDecode() {
-    if (document.getElementById("live").checked == true) {
-        document.getElementById("text").addEventListener("input", encode);
+//Live function doesn't work properly
+/*
+function liveEncodeDecode(mode) {
+    if (mode == "e") {
+        if (document.getElementById("live").checked == true) {
+            document.getElementById("text").addEventListener("input", encode);
+            console.log(document.getElementById("text").value);
+        }
+        else {
+            document.getElementById("text").removeEventListener("input", encode);
+        }
     }
     else {
-        document.getElementById("text").removeEventListener("input", encode);
+        if (document.getElementById("live").checked == true) {
+            document.getElementById("text").addEventListener("input", decode);
+        }
+        else {
+            document.getElementById("text").removeEventListener("input", decode);
+        }
+    }
+}
+*/
+
+function switchEncodeDecode(clicked, mode) {
+    clearFields();
+    var classString = clicked.children[0].className;
+    if (classString != "active") {
+        var newClass = classString.concat("active");
+        clicked.children[0].className = newClass;
+    }
+    if (mode == "e") {
+        document.getElementById("decodeTab").children[0].className = "";
+        //document.getElementById("live").addEventListener("click", function () { liveEncodeDecode("e"); });
+        document.getElementById("encode").addEventListener("click", encode);
+        document.getElementById("encode").innerHTML = "encode";
+        //document.getElementsByClassName("switch")[0].children[0].innerHTML = "Live encode";
+        //document.getElementById("live").checked = false;
+        if (!eventEncode) {
+            //document.getElementById("live").removeEventListener("click", function () { liveEncodeDecode("d"); });
+            document.getElementById("encode").removeEventListener("click", decode);
+            //document.getElementById("live").addEventListener("click", function () { liveEncodeDecode("e"); });
+            document.getElementById("encode").addEventListener("click", encode);
+            eventEncode = true;
+        }
+    }
+    else {
+        document.getElementById("encodeTab").children[0].className = "";
+        //document.getElementById("live").addEventListener("click", function () { liveEncodeDecode("d"); });
+        document.getElementById("encode").addEventListener("click", decode);
+        document.getElementById("encode").innerHTML = "decode";
+        //document.getElementsByClassName("switch")[0].children[0].innerHTML = "Live decode";
+        //document.getElementById("live").checked = false;
+        if (eventEncode) {
+            //document.getElementById("live").removeEventListener("click", function () { liveEncodeDecode("e"); });
+            document.getElementById("encode").removeEventListener("click", encode);
+            //document.getElementById("live").addEventListener("click", function () { liveEncodeDecode("d"); });
+            document.getElementById("encode").addEventListener("click", decode);
+            eventEncode = false;
+        }
     }
 }
 
-document.getElementById("live").addEventListener("click", liveEncodeDecode);
+function clearFields() {
+    document.getElementById("text").value = "";
+    document.getElementById("result").value = "";
+    //document.getElementById("live").checked = false;
+    //liveEncodeDecode("e");
+}
+
+window.addEventListener("load", clearFields);
+//document.getElementById("live").addEventListener("click", function () { liveEncodeDecode("e"); });
 document.getElementById("encode").addEventListener("click", encode);
+document.getElementById("encodeTab").addEventListener("click", function () { switchEncodeDecode(this, "e"); });
+document.getElementById("decodeTab").addEventListener("click", function () { switchEncodeDecode(this, "d"); });
