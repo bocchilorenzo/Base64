f64aef22119ebb06f0a0ff7395ae9f933409e0d3
bocchilorenzo <lorenzobocchi99@yahoo.com>
2020-11-02 22:39:59 +0100

    added base64 url compatibility, added readme, bug fix

---
+++ README.md
@@ -0,0 +1,9 @@
+This encoder/decoder is compatible with UTF-8 characters and uses ony3000's library [base64-converter](https://github.com/ony3000/base64-converter)
+
+## Functionality
+- encode to base64 format
+- decode from base64 format
+- base64 URL compatibility
+- links in the result text are clickable
+- copy button to directly copy the result
+- to switch from encoding to decoding simply switch tab
--- js/main.js
+++ js/main.js
@@ -3,6 +3,37 @@ var eventEncode = true;
 
 var first = 0;
 
+function unescape(str) {
+    return (str + '==='.slice((str.length + 3) % 4))
+        .replace(/-/g, '+')
+        .replace(/_/g, '/')
+}
+
+function escape(str) {
+    return str.replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=/g, '')
+}
+
+function encodeUrl() {
+    var str = document.getElementById("text").value;
+    document.getElementById("result").innerHTML = escape(converter.base64Encode(str));
+}
+
+function decodeUrl() {
+    document.getElementById("result2").innerHTML = anchorme({
+        input: escape(converter.base64Decode(document.getElementById("text2").value)),
+        // use some options
+        options: {
+            attributes: {
+                target: "_blank"
+            },
+            //truncate: 40,
+            endTruncation: true
+        },
+    });
+}
+
 function encode() {
     var str = document.getElementById("text").value;
     document.getElementById("result").innerHTML = converter.base64Encode(str);
@@ -24,19 +55,64 @@ function decode() {
 
 function liveEncodeDecode(mode) {
     if (mode == "e") {
-        if (document.getElementById("live").checked == true) {
-            document.getElementById("text").addEventListener("keyup", encode);
+        if (document.getElementById("live").checked) {
+            if (document.getElementById("url").checked) {
+                document.getElementById("text").removeEventListener("keyup", encode);
+                document.getElementById("text").addEventListener("keyup", encodeUrl);
+                encodeUrl()
+            }
+            else {
+                document.getElementById("text").removeEventListener("keyup", encodeUrl);
+                document.getElementById("text").addEventListener("keyup", encode);
+                encode()
+            }
         }
         else {
+            document.getElementById("text").removeEventListener("keyup", encodeUrl);
             document.getElementById("text").removeEventListener("keyup", encode);
         }
     }
     else {
-        if (document.getElementById("live").checked == true) {
-            document.getElementById("text2").addEventListener("keyup", decode);
+        if (document.getElementById("live").checked) {
+            if (document.getElementById("url").checked) {
+                document.getElementById("text2").removeEventListener("keyup", decode);
+                document.getElementById("text2").addEventListener("keyup", decodeUrl);
+                decodeUrl()
+            }
+            else {
+                document.getElementById("text2").removeEventListener("keyup", decodeUrl);
+                document.getElementById("text2").addEventListener("keyup", decode);
+                decode()
+            }
         }
         else {
+            document.getElementById("text2").removeEventListener("keyup", decodeUrl);
             document.getElementById("text2").removeEventListener("keyup", decode);
+        }
+    }
+}
+
+function urlEncodeDecode(mode) {
+    if (mode == "e") {
+        liveEncodeDecode("e")
+        if (document.getElementById("url").checked) {
+            document.getElementById("encode").removeEventListener("click", encode);
+            document.getElementById("encode").addEventListener("click", encodeUrl);
+        }
+        else {
+            document.getElementById("encode").removeEventListener("click", encodeUrl);
+            document.getElementById("encode").addEventListener("click", encode);
+        }
+    }
+    else {
+        liveEncodeDecode("e")
+        if (document.getElementById("url").checked) {
+            document.getElementById("encode").removeEventListener("click", decode);
+            document.getElementById("encode").addEventListener("click", decodeUrl);
+        }
+        else {
+            document.getElementById("encode").removeEventListener("click", decodeUrl);
+            document.getElementById("encode").addEventListener("click", decode);
         }
     }
 }
@@ -51,27 +127,34 @@ function switchEncodeDecode(clicked, mode) {
         clicked.children[0].className = newClass;
     }
     document.getElementById("live").checked = false;
+    document.getElementById("url").checked = false;
     if (mode == "e") {
         document.getElementById("live").removeEventListener("click", function () { liveEncodeDecode("d"); });
+        document.getElementById("url").removeEventListener("click", function () { urlEncodeDecode("d"); });
         document.getElementById("encode").removeEventListener("click", decode);
         document.getElementById("live").addEventListener("click", function () { liveEncodeDecode("e"); });
+        document.getElementById("url").addEventListener("click", function () { urlEncodeDecode("e"); });
         document.getElementById("encode").addEventListener("click", encode);
         document.getElementById("decodeTab").children[0].className = "";
         document.getElementById("encode").innerHTML = "encode";
         document.getElementsByClassName("switch")[0].children[0].innerHTML = "Live encode";
         document.getElementById("decodeDiv").style.display = "none";
         document.getElementById("encodeDiv").style.display = "block";
+        liveEncodeDecode("e")
     }
     else {
         document.getElementById("live").removeEventListener("click", function () { liveEncodeDecode("e"); });
+        document.getElementById("url").removeEventListener("click", function () { urlEncodeDecode("e"); });
         document.getElementById("encode").removeEventListener("click", encode);
         document.getElementById("live").addEventListener("click", function () { liveEncodeDecode("d"); });
+        document.getElementById("url").addEventListener("click", function () { urlEncodeDecode("d"); });
         document.getElementById("encode").addEventListener("click", decode);
         document.getElementById("encodeTab").children[0].className = "";
         document.getElementById("encode").innerHTML = "decode";
         document.getElementsByClassName("switch")[0].children[0].innerHTML = "Live decode";
         document.getElementById("encodeDiv").style.display = "none";
         document.getElementById("decodeDiv").style.display = "block";
+        liveEncodeDecode("d")
     }
 }
 
@@ -142,6 +225,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 window.addEventListener("load", function () { clearFields("e"); });
 document.getElementById("live").addEventListener("click", function () { liveEncodeDecode("e"); });
+document.getElementById("url").addEventListener("click", function () { urlEncodeDecode("e"); });
 document.getElementById("encode").addEventListener("click", encode);
 document.getElementById("encodeTab").addEventListener("click", function () { switchEncodeDecode(this, "e"); });
 document.getElementById("decodeTab").addEventListener("click", function () { switchEncodeDecode(this, "d"); });
